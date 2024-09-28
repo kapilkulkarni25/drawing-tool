f9610b2c1ec1ba3c44d99fa8bd0fe3e21e38b590
kapilkulkarni25 <kapilkulkarni74@gmail.com>
2024-09-28 22:22:10 +0530

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Menu from "@/components/Menu";
+import Board from "@/components/Board";
+import Toolbox from "@/components/ToolBox";
+
+export default function Home() {
+  return (
+    <>
+      <Board />
+      <Menu />
+      <Toolbox />
+    </>
+  );
+}
