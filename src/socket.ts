7920ebd9e7035b3003c13eb866b4f60d75bfa1d0
kapilkulkarni25 <kapilkulkarni74@gmail.com>
2024-09-28 23:07:39 +0530

    fix: env url

--- src/socket.ts
+++ src/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 const URL =
   process.env.NODE_ENV === "production"
-    ? "https://jscafe-sketchbook-server.onrender.com"
+    ? "https://drawing-tool-backend-vz6p.onrender.com"
     : "http://localhost:5000";
 export const socket = io(URL);
